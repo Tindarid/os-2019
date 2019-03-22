fd7846858796ff9dfbbb4e1da6de155a04ae45c1
Nursan Valeyev <tindarid@gmail.com>
2019-03-22 22:20:05 +0300

    day03 fully resolved

---
+++ lectures/lecture03/src/hole1.c
@@ -0,0 +1,6 @@
+struct hole {
+    uint64_t a;
+    uint32_t b;
+    uint64_t c;
+    uint32_t d;
+}
---
+++ lectures/lecture03/src/hole2.c
@@ -0,0 +1,6 @@
+struct hole {
+    uint64_t a;
+    uint32_t b;
+    uint32_t d;
+    uint64_t c;
+}
